553d9d6dec00ca2f12a79a5d7babfa824f81f384
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-11-22 11:15:52 -0500

    connected to deployed api, build link-to with objects, not ids

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
   namespace: 'api/v1',
-  host: 'http://localhost:3000'
+  host: 'https://mysterious-garden-7912.herokuapp.com'
 });
 
